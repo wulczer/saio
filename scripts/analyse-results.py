db94f8a7bc917c8aec81fdd04aca2079d0cc7ba9
Jan Urbański <wulczer@wulczer.org>
2010-06-15 00:20:46 +0200

    Textual statuses from the analyse script

--- scripts/analyse-results.py
+++ scripts/analyse-results.py
@@ -2,11 +2,19 @@
 
 import sys
 
+statuses = {
+    '0': 'SAIO_MOVE_OK',
+    '1': 'SAIO_MOVE_FAILED_FAST',
+    '2': 'SAIO_MOVE_FAILED',
+    '3': 'SAIO_MOVE_DISCARDED',
+    '4': 'SAIO_MOVE_IMPOSSIBLE'
+}
+
 freqs = {}
 
 for line in sys.stdin:
     num, cost, temp, result, jonrels = line.split()
-    freqs.setdefault(result, 0)
-    freqs[result] += 1
+    freqs.setdefault(statuses[result], 0)
+    freqs[statuses[result]] += 1
 
 print freqs
