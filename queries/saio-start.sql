6d03a049d6d44f65fb40e6c251541f89109b7832
Jan Urbański <wulczer@wulczer.org>
2010-06-19 02:08:58 +0200

    Make the default SAIO parameters saner

--- queries/saio-start.sql
+++ queries/saio-start.sql
@@ -1,6 +1,10 @@
 load 'saio';
+
+set join_collapse_limit to 100;
+set from_collapse_limit to 100;
+
 set saio_algorithm to recalc;
-set saio_equilibrium_factor to 4;
+set saio_equilibrium_factor to 1;
 set saio_initial_temperature_factor to 2.0;
 set saio_temperature_reduction_factor to 0.4;
 set saio_moves_before_frozen to 2;
