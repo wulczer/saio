22d07cc99a80d1d04865f95ab164877f5ffd1771
Jan Urbański <wulczer@wulczer.org>
2010-06-20 17:14:41 +0200

    Ugly way to get per-execution stats

--- scripts/analyse-results.py
+++ scripts/analyse-results.py
@@ -12,9 +12,21 @@ statuses = {
 
 freqs = dict((status, {'count': 0, 'joinrels': 0}) for status in statuses.values())
 
+start_cost = 0
+min_cost = 2**31
+last_name = None
+
 for line in fileinput.input():
-    num, cost, temp, result, joinrels = line.split()
+    if last_name and last_name != fileinput.filename():
+        print "start cost: %.2f, min cost: %.2f" % (start_cost, min_cost)
+        start_cost = 0
+        min_cost = 2**31
+    last_name = fileinput.filename()
+    num, temp, cost, result, joinrels = line.split()
+    start_cost = start_cost or float(cost)
+    min_cost = min(min_cost, float(cost))
     freqs[statuses[result]]['count'] += 1
     freqs[statuses[result]]['joinrels'] += int(joinrels)
 
+print "start cost: %.2f, min cost: %.2f" % (start_cost, min_cost)
 print freqs
