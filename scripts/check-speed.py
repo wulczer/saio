dd5570da0c2be37ffb9374c3f00cafe60110ca31
Jan Urbański <wulczer@wulczer.org>
2010-07-18 23:41:24 +0200

    Test script updates

--- scripts/check-speed.py
+++ scripts/check-speed.py
@@ -1,18 +1,31 @@
 #!/usr/bin/python
 
 import psycopg2
+import optparse
 from xml.dom import minidom
 import time
 import numpy
 import sys
 
-def check(db, query, saio, saio_params):
+tests1 = [
+ (0, 0, 0, 0),
+ (4, 2, 0.6, 2),
+ (6, 2, 0.4, 2),
+ (6, 2, 0.6, 2),
+ (8, 2, 0.4, 2),
+ (12, 2, 0.6, 2),
+ (12, 2, 0.8, 2)
+]
 
-    conn = psycopg2.connect(host="localhost", database=db)
-    conn.set_isolation_level(0)
+tests2 = [(i, 2, j, 2)
+          for i in range(2, 13, 1)
+          for j in numpy.linspace(0.2, 0.9, 11)]
+
+def check_time(conn, query, saio_params):
+
     cur = conn.cursor()
 
-    if saio:
+    if saio_params[0]:
         eq_factor, initial_temp, temp_reduction, before_frozen = saio_params
         cur.execute("load 'saio'")
         cur.execute("set saio_algorithm to recalc")
@@ -36,38 +49,50 @@ def check(db, query, saio, saio_params):
                 "Plan").item(0).getElementsByTagName(
                 "Total-Cost").item(0).childNodes])
     t = t2 - t1
-    return cost, t
+    cur.close()
+    return float(cost), float(t)
+
+def max_memory(pid):
+    for line in file('/proc/%d/status' % pid):
+        if not line.startswith('VmPeak'):
+            continue
+        print line.split()
+        num, unit = line.split()[1:3]
+        return int(num)
 
 
-LOOPS = 10
-
-TEMPLATE = r"""      %s & %s & %s & %f & %.5f \\ \hline"""
-
-def transpose(*args):
-    a, b = zip(*[check(*args) for _ in range(LOOPS)])
-    return numpy.array(map(float, a)), numpy.array(map(float, b))
-
-
-if len(sys.argv) > 1:
-   db, query = "robert", "explain (format xml) select * from foo_view order by name"
-else:
-   db, query = "complex", file("/home/wulczer/saio/queries/explain.sql").read()
-
-
-for saio_params in (
-    (4, 2.0, 0.6, 2),
-    (6, 2.0, 0.4, 2),
-    (6, 2.0, 0.6, 2),
-    (8, 2.0, 0.4, 2),
-    None,
-    ):
-    if not saio_params:
-        costs, times = transpose(
-            db, query, False, None)
-        print TEMPLATE % ("GEQO", "n/a", "n/a",
-                          numpy.average(costs), numpy.average(times))
+def output(path, params, cost, t, memory):
+    if params[0]:
+        p = "(%d, %.3f, %.3f, %d)" % params
     else:
-        costs, times = transpose(
-            db, query, True, saio_params)
-        print TEMPLATE % ("SAIO", str(saio_params[0]), str(saio_params[2]),
-                          numpy.average(costs), numpy.average(times))
+        p = "GEQO"
+    print "Ran tests for %s, got %.5f %.5f %.5f" % (p, cost, t, memory)
+    file(path, 'a').write("%d\t%f\t%f\t%d\t%f\t%f\t%f\n" % (params + (cost, t, memory)))
+
+
+def run_tests(tests, query, path, loops):
+    for params in tests:
+        costs, times, memory = [], [], []
+        for i in range(loops):
+            conn = psycopg2.connect(host="localhost")
+            conn.set_isolation_level(0)
+            pid = conn.get_backend_pid()
+            cost, t = check_time(conn, query, params)
+            memory.append(max_memory(pid))
+            costs.append(cost)
+            times.append(t)
+            conn.close()
+        output(path, params, numpy.average(costs),
+               numpy.average(times), numpy.average(memory))
+
+
+def main():
+    parser = optparse.OptionParser()
+    parser.add_option("-l", "--loops", type=int)
+    opts, args = parser.parse_args(sys.argv)
+
+    tests(tests1, QUERY1, "query1.tests1.out", opts.loops)
+    tests(tests2, QUERY1, "query1.tests2.out", 1)
+
+    tests(tests1, QUERY2, "query2.tests1.out", opts.loops)
+    tests(tests2, QUERY2, "query2.tests2.out", 1)
